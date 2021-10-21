d5b0797a52c73725fe44b3c181f3cfdaf1c6714f
dongzhiwei <2660847388@qq.com>
2021-10-21 14:44:06 +0800

    数据库

--- internal/api/admin.go
+++ internal/api/admin.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"inherited/internal/models"
 	"inherited/internal/services"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,10 +19,20 @@ func CreateAdminUser(ctx *gin.Context) {
 		fmt.Printf("[api.CreateAdminUser], Parameter parsing error")
 	}
 
+	userName := adminUser.UserName
+	password := adminUser.Password
+	if userName == "" || password == "" {
+		log.Printf("userName or password is null")
+
+		return
+	}
+
 	sysUser := new(services.SysUser)
 	err = sysUser.CreateSysUser(adminUser.UserName, adminUser.Password)
 	if err != nil {
 		fmt.Printf("[api.CreateAdminUser], err: %v", err)
+
+		return
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
